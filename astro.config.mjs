e5280d1e8d4a404636aadba788a98300c1d46d76
Matheus Luís <matheus.bernardi@lnls.br>
2025-02-20 15:14:55 -0300

    Add KaTeX and remark-math for math rendering in documentation and guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,22 @@ import react from "@astrojs/react";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   site: "https://matyson.github.io",
   base: "wkb",
   integrations: [
     starlight({
+      components: {
+        PageFrame: "src/components/PageFrame.astro",
+      },
       title: "wkb",
       social: {
         github: "https://github.com/matyson/wkb",
